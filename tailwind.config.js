01f8d9f68a6422b08a345e7e9760f87bdc40c550
fchpriani <sietedosfede@gmail.com>
2021-03-08 11:13:05 -0300

    chore: setup design tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,17 @@ module.exports = {
     content: ["./src/**/*.svelte"],
     enabled: production,
   },
+	theme: {
+		fontFamily: {
+			'bowlby': ['Bowlby One', 'cursive'],
+			'free': ['Free Sans', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+        'pet-black': '#15202B',
+				'pet-blue': '#0c4fec',
+				'pet-gray': '#c8c8c8',
+			},
+		},
+	},
 };
