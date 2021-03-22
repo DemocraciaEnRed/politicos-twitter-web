50cacdc60c07eb806496a767f90b2350f4578bd7
fchpriani <sietedosfede@gmail.com>
2021-03-22 19:43:20 -0300

    feat: update tos

--- src/pages/bases.svelte
+++ src/pages/bases.svelte
@@ -3,7 +3,7 @@
 	import AccordionItem from "./_components/AccordionItem.svelte";
 </script>
 
-<div class="w-full h-screen flex flex-col-reverse lg:flex-col bg-pet-gray">
+<div class="w-full h-screen flex flex-col-reverse lg:flex-col bg-pet-gray texture-gray">
 	<nav>
 		<ul class="w-full p-8 lg:pl-12 flex gap-4 sm:gap-24 lg:justify-around flex-wrap lg:flex-nowrap
 			text-base 2xl:text-xl lg:text-sm font-bowlby text-pet-black uppercase">
@@ -39,8 +39,8 @@
 	</div>
 </div>
 
-<div class="w-full flex flex-col-reverse items-center lg:flex-col bg-pet-gray
-text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
+<div class="w-full py-8 flex flex-col-reverse items-center lg:flex-col bg-pet-gray
+text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue texture-gray">
 	<Accordion>
 		<AccordionItem title="1. PRESENTACIÓN">
 			<p>
@@ -50,6 +50,7 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				queremos invitarlos/as a contribuir en la explotación del repositorio
 				<b>Políticxs en Twitter</b> con el fin de <b>producir conocimiento a partir de datos</b>.
 			</p>
+			<br>
 			<p>
 				<b>Politicxs en Twitter</b> es un repositorio que nuclealas interacciones en Twitter
 				de más de 500 usuarios relacionados con la política. Ponemos a disposición
@@ -67,12 +68,14 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				y analicen, de forma original e innovadora, la responsabilidad de políticos y políticas
 				en Argentina en las dinámicas de la desinformación.
 			</p>
+			<br>
 			<p>
 				La desinformación es un fenómeno político a través del cual se intenta, con diferentes intereses,
 				afectar la conversación pública a partir de la inclusión de mensajes y/o contenidos que no reflejan la realidad.
 				Así, se crean artículos, sitios web y/o publicaciones deliberadamente engañosos que, con frecuencia,
 				procuran infringir un daño en el oponente político.
 			</p>
+			<br>
 			<p>	
 				Consideramos relevante promover la discusión en torno a cómo nos informamos, cómo dirimimos
 				entre contenido legítimo y desinformación, cuáles pueden ser consideradas fuentes verídicas
@@ -80,6 +83,7 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				de los medios de comunicación y del comportamiento inauténtico organizado, incluyendo “trolls”,
 				que impulsan la desinformación. Pero poco se habla de la actitud de nuestros representantes.
 			</p>
+			<br>
 			<p>
 				La <b>presente convocatoria apunta, específicamente, a construir evidencia sobre el rol que juegan
 				los políticos y políticas del país en la proliferación de la desinformación</b>:
@@ -95,21 +99,150 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				En ambos casos solo podrá presentarse una idea por autor o coautores participantes.
 				En caso de autoría colectiva, todos los involucrados deberán aprobar la postulación de la propuesta.				
 			</p>
+			<br>
 			<p>
 				Apuntamos a que participen de esta convocatoria perfiles de investigación:
 				estudiantes, profesionales, académicos, cientistas y visualizadores de datos
 				vinculados al mundo de Ingeniería de Datos aplicada a las Ciencias Sociales.
 			</p>
+			<br>
 			<p>
-				Es una condición para participar la utilización de los datos facilitados en el repositorio de Políticxs en Twitter.
+				Es una condición para participar la utilización de los datos facilitados en el repositorio de <b>Políticxs en Twitter</b>.
 				Cualquier contenido o idea que sea copiada de otras obras,
 				dándolo a entender como propio y sin adjudicar de forma explícita que no es de su autoría, será considerado como plagio.
 				En caso de que se detecte plagio en la parcialidad o en la totalidad de la idea,
 				la misma quedará automáticamente descalificada de la convocatoria. 
 			</p>
+			<br>
+			<p>
+				La convocatoria constará de 4 (cuatro) <b>instancias</b> que son:
+			</p>
+			<br>
+			<ol type="a">
+				<li>
+					<b>A.</b> Recolección de propuestas
+					<br>
+					<br>
+					<p>
+						Se abrirá un formulario en línea en el que deberán presentarse las propuestas en idioma español.
+						En ese formato, se deberán incluir los siguientes elementos listados a continuación de forma obligatoria:
+					</p>
+					<br>
+					<ul>
+						<li>
+							· Nombre del participante(s)
+						</li>
+						<li>
+							· Nombre de un representante del equipo
+						</li>
+						<li>
+							· Contacto de correo electrónico
+						</li>
+						<li>
+							· Antecedentes de la/s persona/s solicitantes para conocer en profundidad las cualidades y experiencia relevante en materia de explotación de datos o relacionada con el tema de la convocatoria. 
+						</li>
+						<li>
+							· Título de la propuesta
+						</li>
+						<li>
+							· Un resumen o abstract de la propuesta de hasta 500 palabras como máximo, incluyendo una breve propuesta metodológica. 
+						</li>
+					</ul>
+					<br>
+					<p>
+						Las propuestas serán evaluadas según los siguientes <b>criterios</b>:
+					</p>
+					<br>
+					<ul>
+						<li>
+							· Pertinencia con respecto al tema de la convocatoria	
+						</li>
+						<li>
+							· Claridad y precisión en la redacción
+						</li>
+						<li>
+							· Originalidad
+						</li>
+						<li>
+							· Rigurosidad en el uso de los datos
+						</li>
+						<li>
+							· El aporte específico al campo de conocimiento 
+						</li>
+					</ul>
+				</li>
+				<li>
+					<br>
+					<b>B.</b> Preselección de propuestas
+					<br><br>
+					<p>
+						Todas las propuestas serán evaluadas por un “Jurado de Evaluación”, integrado por un panel 7 (siete) personas,
+						con capacidades y conocimientos técnicos acordes. El jurado preseleccionará, basándose en los criterios ya mencionados,
+						<b>3 (tres) propuestas</b> con el potencial para ganar la convocatoria. 
+					</p>
+				</li>
+				<li>
+					<br>
+					<b>C.</b> Presentación de las propuestas preseleccionadas
+					<br><br>
+					<p>
+						Lx/s autor/es de las propuestas preseleccionadxs deberán presentarse en una instancia virtual, en fecha a convenir,
+						en la que contarán con <b>10 (diez) minutos</b> máximo para exponer su idea, mostrar las primeras “visualizaciones piloto”
+						y explicar cómo llevarán a cabo su proyecto de investigación. En esta instancia, se evaluará la capacidad de pitcheo,
+						la originalidad de la presentación y el marco técnico brindado. 
+					</p>
+					<p>
+						Las instrucciones sobre la Presentación (fecha, formato, modalidad, etc.)
+						serán enviadas a la casilla de correo electrónico brindada en el formulario, con la debida antelación. 
+					</p>
+				</li>
+				<li>
+					<br>
+					<b>D.</b> Implementación de la idea ganadora
+					<br><br>
+					<p>
+						La idea ganadora recibirá un premio de 140.000 (ciento cuarenta mil pesos)
+						que financiará el desarrollo de la idea presentada hacia un proyecto de investigación. 
+					</p>
+					<p>
+						El proyecto de investigación deberá contar de forma obligatoria con:
+					</p>
+					<br>
+					<ul>
+						<li>
+							· <b>Cuerpo de análisis y desarrollo.</b> Con un máximo de 7.000 palabras (incluidas notas al pie y bibliografía).
+							El formato deberá ser de páginas de tamaño A4, con márgenes de 3 cm. Párrafos sangrados y justificados,
+							con interlineado de 1,5 puntos, sin doble salto de línea entre párrafos.
+							Texto en tipografía serif (Times New Roman u otra), con cuerpo de 12 puntos.
+							<p>
+								Para las citas, se solicita atenerse al
+								<a href="http://iigg.sociales.uba.ar/wp-content/uploads/sites/22/2020/11/LaCita4taed_10_11_20.pdf">
+									estilo APA
+								</a>.
+							</p>
+						</li>
+						<br>
+						<li>
+							· <b>Código y csv trabajados.</b> Sólo se aceptarán investigaciones que hayan creado sus códigos
+							en los programas RStudio o Python. La entrega del  código deberá hacerse en formato txt.,
+							especificando qué programa se utilizó para realizarlo, en .py o un .R. En ambos casos,
+							deberán entregarse todos los archivos (pueden ser txt, csv, excel, geojson, rdata, pkl, etc)  que se utilizaron
+							para hacer la investigación. El código debe estar completo, debe ser autosuficiente y autoexplicativo.
+							Es decir,  debe poder correrse sin ningún tipo de inconveniente al momento de la evaluación por parte del Jurado
+							y debe estar bien documentado. En el caso de qué sea posible deberá estar todo el proyecto
+							-con los respectivos archivos- subidos a Github ó Gitlab. 
+						</li>
+						<br>
+						<li>
+							· <b>Visualizaciones.</b> El cuerpo de análisis y desarrollo entregado deberá estar basado
+							en visualizaciones de datos claras y concisas, que muestren relaciones de interconexión entre las variables analizadas.
+							Las visualizaciones tienen que ser originales y de autoría propia.
+						</li>
+					</ul>
+				</li>		
+			</ol>
 		</AccordionItem>
-		<AccordionItem title="4. PRESENTACIONES SELECCIONADAS"></AccordionItem>
-		<AccordionItem title="5. PREMIOS">
+		<AccordionItem title="4. PREMIOS">
 			<p>
 				Buscamos premiar la idea más original y completa, que se alinee con estas bases mencionadas en el punto 2.
 				El <b>premio</b> consta de la <b>entrega monetaria de 140.000 (ciento cuarenta mil pesos)</b>.
@@ -123,7 +256,7 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				deba incurrir para adjudicarse su premio quedará a su cargo de forma exclusiva. 
 			</p>
 		</AccordionItem>
-		<AccordionItem title="6. PLAZOS">
+		<AccordionItem title="5. PLAZOS">
 			<p>
 				La convocatoria estará abierta desde el 30 de marzo hasta el 24 de abril de 2021 a las 23:59 horas de Argentina.
 				No se aceptarán trabajos enviados luego de esta fecha.
@@ -132,12 +265,13 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				con indicaciones claras sobre los campos requeridos. 
 			</p>
 		</AccordionItem>
-		<AccordionItem title="7. DERECHOS Y PUBLICACIONES">
+		<AccordionItem title="6. DERECHOS Y PUBLICACIONES">
 			<p>
 				Esta convocatoria no tiene ningún tipo de fin de lucro o rédito económico
 				sino que sus únicos fines son la generación de conocimiento colectivo
 				en materia académica y de investigación para su divulgación. 
 			</p>
+			<br>
 			<p>
 				La actual convocatoria se enmarca en el movimiento de Ciencia Abierta.
 				Buscamos producir conocimiento de manera abierta y colaborativa
@@ -146,6 +280,7 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				Es en esta línea que las organizaciones encargadas de la convocatoria ponen a libre disposición y completa accesibilidad
 				datos como recursos compartidos para que sean trabajados por cualquier persona interesada. 
 			</p>
+			<br>
 			<p>
 				El objetivo es que los y las participantes de la convocatoria se sientan cómodxs compartiendo sus propuestas
 				y los eventuales resultados finales de sus investigaciones. Sin embargo, compartir sus propuestas, documentos y resultados
@@ -153,16 +288,18 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				de los derechos de propiedad intelectual, es decir, que no podrán apropiarse, adaptar o publicar libremente las propuestas
 				<b>sin autorización previa de su o sus autores</b> y sin establecer de forma clara los créditos de autoría pertinentes.
 			</p>
+			<br>
 			<p>
 				Todos las propuestas recolectadas para la convocatoria son propiedad exclusiva de sus autores
 				y participar de la misma no requiere la cesión total o parcial de éstas. 
 			</p>
 		</AccordionItem>
-		<AccordionItem title="8. DESCARGO DE RESPONSABILIDAD">
+		<AccordionItem title="7. DESCARGO DE RESPONSABILIDAD">
 			<p>
 				La participación en la convocatoria implica conocimiento y aceptación sin reservas de estas bases y condiciones
 				las cuales regirán la totalidad de las relaciones entre cada participante y las organizaciones en relación a la convocatoria. 
 			</p>
+			<br>
 			<p>
 				Los organizadores no responderán por ningún daño o perjuicio que se ocasione a cualquier persona
 				con motivo o debido a la participación de la misma en la convocatoria, en cualquier calidad que la misma participe,
@@ -170,15 +307,18 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				aquellos daños vinculados con el acceso y otorgamiento de cualquiera de los premios,
 				siempre que dicho daño no sea imputable a la exclusiva culpa clave o exclusivo dolor del organizador. 
 			</p>
+			<br>
 			<p>
 				Los organizadores no serán responsables por daños, perjuicios o pérdidas ocasionadas al ganador/a o cualquier otro tercero,
 				en su persona y / o bienes, con motivo o en ocasión de la aceptación, recepción o utilización del premio. 
 			</p>
+			<br>
 			<p>
 				Toda eventual controversia que, en virtud de esta convocatoria, se genere entre cualquier participante
 				y los organizadores será regida y concertada con total sujeción a las leyes de la República Argentina,
 				renunciando el participante a cualquier otra jurisdicción a cuya aplicación pudiera tener derecho. 
 			</p>
+			<br>
 			<p>
 				Resultarán competentes para dirimir cualquier controversia o procedimiento legal,
 				que surja entre cualquier participante y los organizadores, los tribunales ordinarios de la Ciudad de Buenos Aires,
@@ -186,7 +326,7 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 				que pudiera corresponderle. 
 			</p>
 		</AccordionItem>
-		<AccordionItem title="9. CONSULTAS">
+		<AccordionItem title="8. CONSULTAS">
 			<p>
 				En caso de consultas escribir a XX, con copia a XX.
 			</p>
@@ -232,11 +372,11 @@ text-center text-base 2xl:text-5xl lg:text-sm font-bowlby text-pet-blue">
 	button {
 		@apply p-2 rounded-3xl border-2 border-pet-blue text-pet-blue uppercase;
 	}
-	
+	/*
 	li {
 		list-style: none;
 	}
-
+	*/
 	li > a:hover {
 		@apply text-pet-blue border-pet-blue border-b-4;
 	}
