7260a38f1e4065e947d2d7dd092d5eb38f478f70
fchpriani <sietedosfede@gmail.com>
2021-03-25 15:10:21 -0300

    chore: styles workaround

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,2 +1,22 @@
 <!-- routify:options preload="proximity" -->
 <slot />
+
+<style global>
+  .texture-gray {
+    background-image: url('images/background/gray.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .texture-blue {
+    background-image: url('images/background/blue.png');
+    background-repeat: no-repeat;
+    background-size: cover;		
+  }
+
+  .texture-black {
+    background-image: url('images/background/ocean.png');
+    background-repeat: no-repeat;
+    background-size: cover;		
+  }
+</style>
