6a5f6387744c23382ba330cf8953380a429abaf9
fchpriani <sietedosfede@gmail.com>
2021-03-29 23:33:25 -0300

    fix: workaround for images url

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -3,19 +3,19 @@
 
 <style global>
   .texture-gray {
-    background-image: url('images/background/low-gray.jpg');
+    background-image: url('https://politicosentwitter.com/images/background/low-gray.jpg');
     background-repeat: no-repeat;
     background-size: cover;
   }
 
   .texture-blue {
-    background-image: url('images/background/low-blue.jpg');
+    background-image: url('https://politicosentwitter.com/images/background/low-blue.jpg');
     background-repeat: no-repeat;
     background-size: cover;		
   }
-
+  
   .texture-black {
-    background-image: url('images/background/low-ocean.jpg');
+    background-image: url('https://politicosentwitter.com/images/background/low-ocean.jpg');
     background-repeat: no-repeat;
     background-size: cover;		
   }
