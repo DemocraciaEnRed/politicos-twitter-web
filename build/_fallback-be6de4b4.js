4da42a8547eff1455a2b4facf8df04e231dcba8b
Julieta Garcia <julieta.garcia96@gmail.com>
2021-06-25 10:39:41 -0300

    Updates

--- build/_fallback-be6de4b4.js
+++ build/_fallback-be6de4b4.js
@@ -1,2 +1,2 @@
 import{S as s,i as a,s as e,e as t,a as n,t as r,b as o,c,d as i,n as l,f as u,g as d,u as f}from"./main.js";function v(s){let a,e,d,f,v,x,g,h;return{c(){a=t("div"),e=t("div"),e.textContent="404",d=n(),f=t("div"),v=r("Page not found. \r\n  \r\n  "),x=t("a"),g=r("Go back"),o(e,"class","huge svelte-9lxw8q"),o(x,"href",h=s[0]("../")),o(f,"class","big"),o(a,"class","e404 svelte-9lxw8q")},m(s,t){c(s,a,t),i(a,e),i(a,d),i(a,f),i(f,v),i(f,x),i(x,g)},p(s,[a]){1&a&&h!==(h=s[0]("../"))&&o(x,"href",h)},i:l,o:l,d(s){s&&u(a)}}}function x(s,a,e){let t;return d(s,f,(s=>e(0,t=s))),[t]}export default class extends s{constructor(s){super(),a(this,s,x,v,e,{})}}
-//# sourceMappingURL=_fallback-e1bc0cf7.js.map
+//# sourceMappingURL=_fallback-be6de4b4.js.map
